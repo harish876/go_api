930a7b61230ddb89b0f07429852bbdeb4ed208a4
harish-gokul01 <harish.gokul@byjus.com>
2023-01-04 12:07:22 +0530

    style: :art:

--- server/main.go
+++ server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	app := fiber.New()
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
